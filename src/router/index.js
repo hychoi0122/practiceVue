ab5533d90eb90ef887df65e724430587e740769d
HY CHOI <chlghdud17@gmail.com>
2021-05-18 15:54:10 +0900

    1. menu.js 구성 ( 처리 부분 따로 js처리 ) 추후 json으로 변경 예정

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import bootstrap from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import menuList from '@/routerPath/menu'
+
 
 Vue.use(Router)
 Vue.use(bootstrap)
@@ -23,35 +25,10 @@ const router = new Router({
             path:'/menu',
             name:'menu',
             component : ()=> import('@/components/menu'),
-            children : [
-                { path:'menu1' , component : ()=> import('@/components/menu1')},
-                { path:'menu2' , component : ()=> import('@/components/menu2')},
-                { path:'menu3' , component : ()=> import('@/components/menu3')},
-                { path:'menu4' , component : ()=> import('@/components/menu4')},
-            ]
+            children : menuList
+
         },
-        // {
-        //     path:'/menu/menu1',
-        //     name:'menu1',
-        //     component : ()=> import('@/components/menu1')
-        // },
-        //
-        // {
-        //     path:'/menu/menu2',
-        //     name:'menu2',
-        //     component : ()=> import('@/components/menu2')
-        // },
-        // {
-        //     path:'/menu/menu3',
-        //     name:'menu3',
-        //     component : ()=> import('@/components/menu3')
-        // },
-        // {
-        //     path:'/menu/menu4',
-        //     name:'menu4',
-        //     component : ()=> import('@/components/menu4')
-        // },
-        //board
+
 
     ]
 
---
+++ src/routerPath/menu.js
@@ -0,0 +1,8 @@
+const menu = [
+    { path:'menu1' , component : ()=> import('@/components/menu1')},
+    { path:'menu2' , component : ()=> import('@/components/menu2')},
+    { path:'menu3' , component : ()=> import('@/components/menu3')},
+    { path:'menu4' , component : ()=> import('@/components/menu4')},
+]
+
+export default menu
